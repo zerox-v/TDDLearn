58e7c5c53040a4af92ad2219d9371671e4cbcd09
张选志 <zhangxuanzhi@outlook.com>
2019-08-11 23:05:10 +0800

    完善代码

--- FizzBuzzSolu/TDDLearn/FizzBuzz.cs
+++ FizzBuzzSolu/TDDLearn/FizzBuzz.cs
@@ -4,25 +4,16 @@ namespace FizzBuzz
 {
     public class FizzBuzz
     {
+
         public string Print(int i)
         {
-
-            if (i % 3 == 0 && i % 5 == 0)
-            {
-                return "FizzBuzz";
-            }
-            else if (i % 3 == 0)
-            {
-                return "Fizz";
-            }
-            else if (i % 5 == 0)
-            {
-                return "Buzz";
-
-            }
-            return i.ToString();
+            var result = "";
+            if (i % 3 == 0 || i.ToString().Contains("3")) result += "Fizz";
+            if (i % 5 == 0|| i.ToString().Contains("5") ) result += "Buzz";
+            if (result == "") result = i.ToString();
+            return result;
         }
 
-
+       
     }
 }
--- FizzBuzzSolu/XUnitTestProject1/FizzBuzzUnitTest.cs
+++ FizzBuzzSolu/XUnitTestProject1/FizzBuzzUnitTest.cs
@@ -8,7 +8,11 @@ namespace XUnitTestProject
         [Theory]
         [InlineData(1, "1")]
         [InlineData(3, "Fizz")]
+        [InlineData(13, "Fizz")]
         [InlineData(5, "Buzz")]
+        [InlineData(15, "FizzBuzz")]
+        [InlineData(35, "FizzBuzz")]
+        [InlineData(51, "FizzBuzz")]
         public void Test1(int i, string str)
         {
             var fuzzBuzz = new FizzBuzz.FizzBuzz();
